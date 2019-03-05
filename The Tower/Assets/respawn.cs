efe397831dc97992bccf5a9dbefb05b57f28a3ed
cristian tapia <cristiantapia@cristians-MBP-2.csusm.edu>
2019-03-05 14:23:51 -0800

    test

---
+++ The Tower/Assets/respawn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawn : MonoBehaviour
+{
+    [SerializeField]
+    private Transform player;
+    [SerializeField]
+    private Transform respawn_point;
+
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        player.transform.position = respawn_point.transform.position;
+    }
+}
