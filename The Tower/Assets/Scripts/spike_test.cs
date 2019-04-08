8dd6aef5a4da12f465d5ba56db628db6ff218047
cristian tapia <cristiantapia@cristians-MacBook-Pro-2.local>
2019-04-08 16:27:54 -0700

    progress

---
+++ The Tower/Assets/Scripts/spike_test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spike_test : MonoBehaviour
+{
+    // Start is called before the first frame updat
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime);
+     
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("OutOfBound"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+}
