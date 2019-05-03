196fc1c4ecabdcb751a54a3abf7ed15636d2dee1
Erik Anderson <anderikson1@gmail.com>
2019-05-02 18:04:02 -0700

    Added third level

--- The Tower/Assets/Scripts/FinalScore.cs
+++ The Tower/Assets/Scripts/FinalScore.cs
@@ -5,6 +5,9 @@ using UnityEngine.UI;
 
 public class FinalScore : MonoBehaviour
 {
+    const int BASE_SCORE = 10000;
+    const int CHERRY_MULTIPLIER = 200;
+    const int DEATH_MULTIPLIER = 200;
 
     public Text winText;
     private int playerScore;
@@ -17,7 +20,12 @@ public class FinalScore : MonoBehaviour
 
     void OnEnable()
     {
-        playerScore = PlayerPrefs.GetInt("score");
-        winText.text = "You Win! \nScore: " + playerScore;
+        int cherryCount = PlayerPrefs.GetInt("cherries");
+        int deathCount = PlayerPrefs.GetInt("deaths");
+        int finalScore = BASE_SCORE + cherryCount * CHERRY_MULTIPLIER - deathCount * DEATH_MULTIPLIER;
+       
+        winText.text = "Cherries: " + cherryCount.ToString() +
+                        "\nDeaths: " + deathCount.ToString() +
+                        "\nFinal Score: " + string.Format("{0:n0}", finalScore.ToString());
     }
 }
